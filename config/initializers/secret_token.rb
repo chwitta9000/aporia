3930d03cae9683cc08e087d8791953eab928e935
Charles Whittaker <charles.whittaker@gmail.com>
2013-12-09 16:54:13 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aporia::Application.config.secret_token = 'ff4d2c6b35b04e752e7612f264af33c316f351af07a6c7ec704c1e89b673a2bfd210d88b212d01b25c10a98f2319f70511b6d17e9b78bd60a6308225f3084c3c'
